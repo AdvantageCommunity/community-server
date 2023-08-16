3d13e3f5221932a2b615d257671ad5b2e78f79d6
Shakir Farhan <shakirfarhan123@gmail.com>
2023-08-16 23:24:11 +0530

    minor changes

--- models/users.js
+++ models/users.js
@@ -8,41 +8,41 @@ const userSchema = new mongoose.Schema({
     type: String,
     minLength: 2,
   },
-  phone: {
-    countryCode: Number,
-    phoneNumber: Number,
-  },
-  dateOfBirth: {
-    type: Date,
-  },
-  gender: {
-    type: String,
-    enum: ['male', 'female', 'other'],
-  },
-  bio: {
-    type: String,
-  },
-  username: {
-    type: String,
-    trim: true,
-    // unique: true,
-  },
+  // phone: {
+  //   countryCode: Number,
+  //   phoneNumber: Number,
+  // },
+  // dateOfBirth: {
+  //   type: Date,
+  // },
+  // gender: {
+  //   type: String,
+  //   enum: ['male', 'female', 'other'],
+  // },
+  // bio: {
+  //   type: String,
+  // },
+  // username: {
+  //   type: String,
+  //   trim: true,
+  //   // unique: true,
+  // },
   email: {
     type: String,
     required: true,
     trim: true,
     unique: true,
   },
-  password: {
-    type: String,
-    // required: true,
-    trim: true,
-  },
-  intrests: [
-    {
-      type: String,
-    },
-  ],
+  // password: {
+  //   type: String,
+  //   // required: true,
+  //   trim: true,
+  // },
+  // intrests: [
+  //   {
+  //     type: String,
+  //   },
+  // ],
 });
 const User = mongoose.model('User', userSchema);
 export default User;
