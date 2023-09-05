2c2a9ff22e7df9d479edf635f34d23d9c80b8d0e
Shakir Farhan <shakirfarhan123@gmail.com>
2023-09-05 22:33:23 +0530

    like/unlike and comment/delete comment added

--- controllers/user/blog/blog.js
+++ controllers/user/blog/blog.js
@@ -1,6 +1,7 @@
 import slugify from 'slugify';
 import Blog from '../../../models/blog.js';
 import { uploadToS3 } from '../../../connections/aws.js';
+import { v4 as uuidv4 } from 'uuid';
 export const postBlog = async (req, res) => {
   const { title, content, tags } = req.body;
   const coverImage = req.file;
@@ -81,6 +82,132 @@ export const deleteBlog = async (req, res) => {
         .status(400)
         .json({ message: 'Something Went Wrong. Try Again!' });
     return res.status(200).json({ message: 'Blog deleted successfully.' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const likeABlog = async (req, res) => {
+  const { blogId } = req.params;
+  const userId = req.rootUser._id;
+  if (!blogId) return res.status(400).json({ message: 'Provide blog id.' });
+  try {
+    const blogToLike = await Blog.findOne({ _id: blogId });
+    if (!blogToLike)
+      return res.status(404).json({ message: 'Blog not found.' });
+    if (
+      !blogToLike.likes.some(
+        (like) => like._id.toString() === userId.toString()
+      )
+    ) {
+      blogToLike.likes.push({ _id: userId, createdAt: new Date() });
+      await blogToLike.save();
+      return res
+        .status(400)
+        .json({ message: 'Blog liked successfully', likes: blogToLike.likes });
+    } else {
+      return res
+        .status(400)
+        .json({ message: 'You have already liked this blog' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const unLikeABlog = async (req, res) => {
+  const { blogId } = req.params;
+  const userId = req.rootUser._id;
+  if (!blogId) return res.status(400).json({ message: 'Provide blog id.' });
+  try {
+    const blogToUnLike = await Blog.findOne({ _id: blogId });
+    if (!blogToUnLike)
+      return res.status(404).json({ message: 'Blog not found.' });
+    if (
+      blogToUnLike.likes.some(
+        (like) => like._id.toString() === userId.toString()
+      )
+    ) {
+      const likeIdex = blogToUnLike.likes.findIndex(
+        (like) => like._id.toString() === userId.toString()
+      );
+      const selectedLike = blogToUnLike.likes[likeIdex];
+      if (selectedLike.user.toString() === userId.toString()) {
+        if (likeIdex === -1)
+          return res
+            .status(404)
+            .json({ message: 'You have not liked this blog' });
+        blogToUnLike.likes.splice(likeIdex, 1);
+        await blogToUnLike.save();
+        return res.status(400).json({
+          message: 'Blog unliked successfully',
+          likes: blogToUnLike.likes,
+        });
+      } else {
+        return res
+          .status(403)
+          .json({ message: 'Your not allowed to unlike this blog' });
+      }
+    } else {
+      return res.status(400).json({ message: 'You have not liked this blog' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const commentOnBlog = async (req, res) => {
+  const { blogId } = req.params;
+  const { content } = req.body;
+  if (!blogId) return res.status(400).json({ message: 'Provide blog id' });
+  if (!content) return res.status(400).json({ message: 'Provide content' });
+
+  try {
+    const blog = await Blog.findOne({ _id: blogId });
+    if (!blog) return res.status(404).json({ message: 'Blog not found.' });
+    const comment = {
+      user: req.rootUser._id,
+      content,
+    };
+    blog.comments.push(comment);
+    await blog.save();
+    const savedComment = blog.comments[blog.comments.length - 1];
+    res.status(201).json({ message: 'Comment Added!', comment: savedComment });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const deleteComment = async (req, res) => {
+  const { blogId } = req.params;
+  const { commentId } = req.body;
+  if (!blogId) return res.status(400).json({ message: 'Provide blog id ' });
+  if (!commentId)
+    return res.status(400).json({ message: 'Provide comment id' });
+  try {
+    const blog = await Blog.findOne({ _id: blogId });
+    if (!blog) return res.status(404).json({ message: 'Blog not found' });
+    if (
+      blog.comments.some(
+        (comment) => comment.user.toString() === req.rootUser._id.toString()
+      )
+    ) {
+      let commentIdex = blog.comments.findIndex(
+        (comment) => comment._id.toString() === commentId.toString()
+      );
+      const selectedComment = blog.comments[commentIdex];
+      if (selectedComment.user.toString() === req.rootUser._id.toString()) {
+        console.log(commentIdex);
+        if (commentIdex === -1)
+          return res.status(404).json({ message: 'Comment not found' });
+        blog.comments.splice(commentIdex, 1);
+        await blog.save();
+        return res.status(200).json({ message: 'Comment deleted' });
+      } else {
+        return res
+          .status(403)
+          .json({ message: 'Your not allowed to delete this comment' });
+      }
+    } else {
+      return res.status(404).json({ message: 'Comments Not Found' });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- routes/user/blog.js
+++ routes/user/blog.js
@@ -3,6 +3,10 @@ import {
   postBlog,
   updateBlog,
   deleteBlog,
+  likeABlog,
+  commentOnBlog,
+  unLikeABlog,
+  deleteComment,
 } from '../../controllers/user/blog/blog.js';
 import { isUserAuthenticated } from '../../middleware/user.js';
 import { upload } from '../../connections/aws.js';
@@ -11,5 +15,9 @@ const router = express.Router();
 router.post('/me', upload.single('coverImage'), isUserAuthenticated, postBlog);
 router.patch('/me/:slug', isUserAuthenticated, updateBlog);
 router.delete('/me/:slug', isUserAuthenticated, deleteBlog);
+router.post('/:blogId/like', isUserAuthenticated, likeABlog);
+router.delete('/:blogId/like', isUserAuthenticated, unLikeABlog);
+router.post('/:blogId/comment', isUserAuthenticated, commentOnBlog);
+router.delete('/:blogId/comment', isUserAuthenticated, deleteComment);
 
 export default router;
