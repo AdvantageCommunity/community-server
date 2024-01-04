4f8bbe841dff066f98f8bf587af692dea924a9c6
Shakir Farhan <shakirfarhan123@gmail.com>
2024-01-04 10:49:15 +0530

    redis

--- connections/redis.js
+++ connections/redis.js
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
 export const redis = new Redis({
-  host: 'rediss://red-cmb3o2md3nmc73emgue0:5Y6Z7aRY4NmtJfexoooTAJmNEFhjPQu5@oregon-redis.render.com',
+  host: 'oregon-redis.render.com',
   port: 6379,
 });
