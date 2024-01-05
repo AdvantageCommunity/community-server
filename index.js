1c6182f774fd8b42fcdc34558d5d4443521324f7
Shakir Farhan <shakirfarhan123@gmail.com>
2024-01-05 14:57:05 +0530

    cors fix attempt

--- index.js
+++ index.js
@@ -17,23 +17,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 // MongoDB connection
-// const corsOptions = {
-//   origin: [
-//     'https://dashboard-advantagecommunity.onrender.com',
-//     'https://advantagecommunity.in',
-//     'http://localhost:3000',
-//   ],
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   allowedHeaders: 'Content-Type,Authorization',
-//   credentials: true,
-// };
-app.use(
-  cors({
-    origin: 'https://dashboard-advantagecommunity.onrender.com',
-    // methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    'https://dashboard-advantagecommunity.onrender.com',
+    'https://advantagecommunity.in',
+    'http://localhost:3000',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  // credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
